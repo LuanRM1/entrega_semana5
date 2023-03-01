8eb413f9adf4979e99f6e663a817c99fd5f0170d
LuanRM1 <luan.mello@sou.inteli.edu.br>
2023-03-01 13:27:12 -0300

    xx
    d

--- Node2D.gd
+++ Node2D.gd
@@ -26,9 +26,9 @@ func _on_Button3_pressed():
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
 	while(len(lista)>i):  #while estava sem a comparação 
-		if(lista[i]%2!=0):  
+		if(lista[i]%2!=0):  #troquei o parametro de comparação para diferente de 0
 			cont+=1
-		i+=1
+		i+=1				#adicionei uma incrementação de variavel
 	if(cont!=0):   #indentação do if estava errada
 		nome = nome+"baldo"
 		$Label2.text = nome
